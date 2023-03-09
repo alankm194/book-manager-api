419a8c9dd1f3f660a8989a44bcc18e921b5e708f
alankm194 <alankm194@gmail.com>
2023-03-09 21:04:24 +0000

    fix: weird bug that makes test fails when either running consecutively or when running individually
    - the fix forces the tests to reset context after every test.
    
    - This is due to BookManagerRepository keeping context when running consecutively and having more test data saved in the repository. while during an individual run the BookManagerRepository is fresh and will then create a book using its own auto generated id and ignore the precreated id. therefore setting book id to 1L will fix the issue as that is equal to the autogenerated id Spring JPA creates.

--- src/test/java/com/techreturners/bookmanager/repository/BookManagerRepositoryTests.java
+++ src/test/java/com/techreturners/bookmanager/repository/BookManagerRepositoryTests.java
@@ -3,13 +3,16 @@ package com.techreturners.bookmanager.repository;
 import com.techreturners.bookmanager.model.Book;
 import com.techreturners.bookmanager.model.Genre;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class BookManagerRepositoryTests {
 
     @Autowired
@@ -20,7 +23,6 @@ public class BookManagerRepositoryTests {
 
         Book book = new Book(1L, "Book One", "This is the description for Book One", "Person One", Genre.Education);
         bookManagerRepository.save(book);
-
         Iterable<Book> books = bookManagerRepository.findAll();
         assertThat(books).hasSize(1);
 
@@ -31,13 +33,12 @@ public class BookManagerRepositoryTests {
 
         Book book = new Book(2L, "Book Two", "This is the description for Book Two", "Person Two", Genre.Fantasy);
         bookManagerRepository.save(book);
-
         var bookById = bookManagerRepository.findById(book.getId());
         assertThat(bookById).isNotNull();
     }
     @Test
     public void testCreateAndDeleteBook() {
-        Book book = new Book(1L, "Book Three", "This is the description for Book Two", "Person Two", Genre.Fantasy);
+        Book book = new Book(1L, "Book Four", "This is the description for Book Two", "Person Four", Genre.Fantasy);
         bookManagerRepository.save(book);
         bookManagerRepository.deleteById(book.getId());
         bookManagerRepository.existsById(book.getId());
